5b51693a7012e7417a7d45c56edfce5bdb03c62d
lucciano01 <lucciano01@hotmail.com>
2023-12-07 20:20:40 -0300

    configurando jenkins file

--- src/main/java/catolica/mini/curso/jenkins/CursoJenkinsApplication.java
+++ src/main/java/catolica/mini/curso/jenkins/CursoJenkinsApplication.java
@@ -2,9 +2,16 @@ package catolica.mini.curso.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CursoJenkinsApplication {
+public class CursoJenkinsApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CursoJenkinsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJenkinsApplication.class, args);
